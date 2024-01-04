4a265657bd38b9e5323d54aab92e06b5ad3e246c
AlexandreRCS <ale.maresias@gmail.com>
2024-01-03 22:39:45 -0300

    Js
    Inicio da herança em  javascript

---
+++ js/favorites.js
@@ -0,0 +1,31 @@
+// COMO OS DADOS SERÃO ESTRUTURADOS
+
+export class Favorites {
+    constructor(root){
+        this.root = document.querySelector(root)
+    }
+}
+
+// CLASS QUE VAI CRIAR A VISUALIZAÇÃO E EVENTO DO HTML
+
+export  class FavoritesView extends Favorites {
+    constructor(root){
+        super(root)
+
+        this.update()
+    }
+
+    update(){
+        this.removeAllTr()
+    }
+
+    removeAllTr(){
+        const tbody = this.root.querySelector('table tbody')
+
+        tbody.querySelectorAll('tr')
+        .forEach((tr) => {
+            tr.remove()
+        })
+      
+    }
+}
