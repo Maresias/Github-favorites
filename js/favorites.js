fe485519fe1988f4d27253b991ab9f4650d00d2f
AlexandreRCS <ale.maresias@gmail.com>
2024-02-06 22:08:50 -0300

    updates.favorites

--- js/favorites.js
+++ js/favorites.js
@@ -57,6 +57,15 @@ export class Favorites {
 
     async add(username){
         try{
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            console.log(userExists)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
             const user = await githubUser.search(username)
 
             if(user.login ===  undefined){
@@ -114,6 +123,7 @@ export  class FavoritesView extends Favorites {
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
             row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user p').href = `https://github.com/${user.login}`
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
